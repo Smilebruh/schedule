e0162362c80add21be2b96ee6447c28086218f1e
smilebruh <smilehacking27@gmail.com>
2025-05-05 11:54:06 -0400

    add some feature to add note

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,20 +1,36 @@
 import "./HomePage.css"
+import {useState} from 'react';
+
+const color = [
+    "red",
+    "green",
+    "blue",
+    "yellow",
+    "purple",
+    
+]
 
 function HomePage(){
+    const [notes, addNotes] = useState([]);
     return(     
         <div className="Parent">
             <nav>
-            <button className="add">+</button>
-
-
-            
+                <button onClick={()=>{
+                    let colorindex = Math.floor(Math.random() * 5);
+                    let currentdivcolor = color[colorindex];
+                    let div = <div style={{backgroundColor:currentdivcolor}} className="item">hai</div>
+                    let newarr = [...notes,div]
+                    addNotes(newarr);
+                }} className="add">+</button>  
             </nav>
+
             <main> 
-                <div style={{backgroundColor : "red"}} className="item">1</div>
+                {notes}
+                {/* <div style={{backgroundColor : "red"}} className="item">1</div>
                 <div style={{backgroundColor : "blue"}} className="item">2</div>
                 <div style={{backgroundColor : "green"}} className="item">3</div>
                 <div style={{backgroundColor : "yellow"}} className="item">4</div>
-                <div style={{backgroundColor : "orange"}} className="item">5</div>
+                <div style={{backgroundColor : "orange"}} className="item">5</div> */}
             </main>
 
         </div>
