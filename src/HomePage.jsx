8d6472f0980784c80e6f71c048058967d5555268
smilebruh <smilehacking27@gmail.com>
2025-05-05 00:00:28 -0400

    add button

---
+++ src/HomePage.jsx
@@ -0,0 +1,24 @@
+import "./HomePage.css"
+
+function HomePage(){
+    return(     
+        <div className="Parent">
+            <nav>
+            {/* <div className="add">+</di> */}
+            <button className="add">add</button>
+
+            
+            </nav>
+            <main> 
+                <div style={{backgroundColor : "red"}} className="item">1</div>
+                <div style={{backgroundColor : "blue"}} className="item">2</div>
+                <div style={{backgroundColor : "green"}} className="item">3</div>
+                <div style={{backgroundColor : "yellow"}} className="item">4</div>
+                <div style={{backgroundColor : "orange"}} className="item">5</div>
+            </main>
+
+        </div>
+)
+}
+
+export default HomePage;
